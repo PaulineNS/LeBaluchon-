f5b62da002800cda01db70603b5562ffa6c9b812
Pauline <pauline.nomballais@hotmail.fr>
2019-08-14 17:42:28 +0200

    Creation of the application and interface

---
+++ LeBaluchonApp/Controller/Currency.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeBaluchonApp
+//
+//  Created by Pauline Nomballais on 14/08/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import UIKit
+
+class Currency: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
