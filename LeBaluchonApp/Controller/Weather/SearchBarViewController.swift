f1dc73fdfb2196cf33c7c27b1842da221ca922fb
Pauline <pauline.nomballais@hotmail.fr>
2019-09-11 15:46:53 +0200

    Created the TableViewController

---
+++ LeBaluchonApp/Controller/Weather/SearchBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SearchBarViewController.swift
+//  LeBaluchonApp
+//
+//  Created by Pauline Nomballais on 11/09/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private var list = [List]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+        let cityList = list[indexPath.row]
+        
+        cell.textLabel?.text = cityList.name
+        
+        return cell
+    }
+    
+    
+
+
+}
