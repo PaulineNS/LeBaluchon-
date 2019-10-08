b8d7961c5ab2ac969dccefe480bc6d37f641db16
Pauline <pauline.nomballais@hotmail.fr>
2019-10-08 11:47:34 +0200

    Creation of my citiesTableView

--- LeBaluchonApp/Weather/Controller/SearchBarViewController.swift
+++ LeBaluchonApp/Weather/Controller/SearchBarViewController.swift
@@ -8,34 +8,32 @@
 
 import UIKit
 
-class SearchBarViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var citiesTableView: UITableView!
     
     let citiesArray = ["Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Montpellier", "Strasbourg", "Bordeaux", "Lille", "New York", "Los Angeles", "Chicago", "Houston", "Philadelphie", "San Diego", "Dallas", "Phoenix", "San Antonio", "San José"]
     
-    private var list = [List]()
-    
-    @IBOutlet weak var cityTableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        cityTableView.dataSource = self
-        WeatherService.shared.getCity { (city) in
-            self.list = city?.list ?? [List]()
-            self.cityTableView.reloadData()
-        }
+        citiesTableView.delegate = self
+        citiesTableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return citiesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = cityTableView.dequeueReusableCell(withIdentifier: "cell") {
-            cell.textLabel?.text = list[indexPath.row].name
-            
-            return cell
-        }
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        cell.textLabel?.text = citiesArray[indexPath.row]
+        
+        return cell
     }
 }
 
