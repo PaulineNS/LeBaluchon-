1ef97928671023e1cd509e7bd99623a6e2ae93f9
Pauline <pauline.nomballais@hotmail.fr>
2019-10-09 21:05:44 +0200

    Creation of FakeResponseData and URLSesionFake for Weather

---
+++ WeatherServiceTestsCase/FakeResponseData.swift
@@ -0,0 +1,9 @@
+//
+//  FakeResponseData.swift
+//  WeatherServiceTestsCase
+//
+//  Created by Pauline Nomballais on 09/10/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherServiceTestsCase/URLSessionFake.swift
@@ -0,0 +1,9 @@
+//
+//  URLSessionFake.swift
+//  WeatherServiceTestsCase
+//
+//  Created by Pauline Nomballais on 09/10/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import Foundation
